bb2342f19ed14e71d3abc7c3dd9c1bc5ecfb476a
HamdiAbraham <64348632+hamdiabraham@users.noreply.github.com>
2020-12-06 22:14:10 +0700

    add smoth scrolling using jquery

--- vendor/navbar.js
+++ vendor/navbar.js
@@ -4,3 +4,17 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+$(".link-scroll").on("click", function (e) {
+  var tujuan = $(this).attr("href");
+  var elementTujuan = $(tujuan);
+  $("html, body").animate(
+    {
+      scrollTop: elementTujuan.offset().top - 50,
+    },
+    1000,
+    "swing"
+  );
+
+  e.preventDefault();
+});
